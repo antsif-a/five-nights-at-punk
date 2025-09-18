c7b91ce4a92219669b397d5af269a3d2caea94a8
Андрей Анциферов < >
2025-09-18 21:09:30 +0300

    asdas

---
+++ scripts/edick-faces-switch.gd
@@ -0,0 +1,22 @@
+extends AnimatedSprite2D
+
+func _ready() -> void:
+	$rest.connect("timeout", restTimeout)
+	$twitch.connect("timeout", twitchTimeout)
+	restStart()
+
+func restTimeout() -> void:
+	frame = randi_range(1, 4)
+	twitchStart()
+	
+func twitchTimeout() -> void:
+	frame = 0
+	restStart()
+
+func restStart() -> void:
+	$rest.wait_time = randf_range(1.0, 5.0)
+	$rest.start()
+	
+func twitchStart() -> void:
+	$twitch.wait_time = randf_range(0.1, 0.4)
+	$twitch.start()
